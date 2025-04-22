e91abcbbd6e6c493cd2b716d935ea2287b75c501
Matej DB <mdrazi01@fesb.hr>
2025-04-22 17:57:58 +0200

    script and delete counter contract

---
+++ script/NukeTheSupply.s.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {NukeTheSupply} from "../src/NukeTheSupply.sol";
+
+contract CounterScript is Script {
+    NukeTheSupply public counter;
+
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+
+        address OWNER = vm.envAddress("OWNER");
+        address WETH = vm.envAddress("WETH");
+        address UNISWAP_SWAP_ROUTER = vm.envAddress("UNISWAP_SWAP_ROUTER");
+
+        NukeTheSupply NTS = new NukeTheSupply(OWNER, WETH, UNISWAP_SWAP_ROUTER);
+        console.log("Counter deployed to:", address(NTS));
+
+        vm.stopBroadcast();
+    }
+}
